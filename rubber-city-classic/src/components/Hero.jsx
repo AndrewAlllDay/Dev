f0a8ffea59ccadc6e57308b428b71a90080ba054
AndrewAlllDay <andrewsnyderdesign@gmail.com>
2024-08-26 15:34:25 -0400

    Part of Initial Build

---
+++ rubber-city-classic/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className='bg-[url("https://images.unsplash.com/photo-1525905579391-c020c6c2cc1f?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] h-screen w-full brightness-75'></div>
+    </div>
+  
+  );
+};
+
+export default Hero
