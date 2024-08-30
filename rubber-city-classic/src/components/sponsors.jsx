7fc9d3456b71fe1c1d8aafcfd0a8c4acc7f733f1
“AndrewAlllDay” <andrewsnyderdesign@gmail.com>
2024-08-30 19:19:00 -0400

    Update sponsors.jsx

--- rubber-city-classic/src/components/sponsors.jsx
+++ rubber-city-classic/src/components/sponsors.jsx
@@ -4,22 +4,22 @@ const sponsors = () => {
     return (
         <div class="relative bg-cityblue h-screen text-white overflow-hidden">
             <h1 className='text-8xl uppercase font-bold mt-36 pl-6'> Sponsors</h1>
-            <div class="container mx-auto mt-24 p-4">
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-cityblue">
+            <div class="container mx-auto mt-20 p-4">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-cityblue">
                     <div class="bg-white p-6 rounded-lg shadow-lg">
                         <img className='w-[60%] mx-auto' src='/images/RCC Logo.png' />
-                        <h2 class="text-lg font-bold mb-2">Title Sponsor</h2>
+                        <h2 class="text-xl font-bold mb-2 text-center uppercase mt-6">Title Sponsor</h2>
                     </div>
                     <div class="bg-white p-6 rounded-lg shadow-lg">
                         <img className='w-[60%] mx-auto' src='/images/RCC Logo.png' />
-                        <h2 class="text-lg font-bold mb-2">Silver Level Sponsor</h2>
+                        <h2 class="text-xl font-bold mb-2 text-center uppercase mt-6">Silver Level Sponsor</h2>
                     </div>
                     <div class="bg-white p-6 rounded-lg shadow-lg">
                         <img className='w-[60%] mx-auto' src='/images/RCC Logo.png' />
-                        <h2 class="text-lg font-bold mb-2"></h2>
+                        <h2 class="text-xl font-bold mb-2"></h2>
                     </div>
                 </div>
-            </div>
+            </div>xl
         </div>
     )
 }
